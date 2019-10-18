8bda9d96c495c1082e37c339a2816e352e288efd
raynegomes <raynegomes@gmail.com>
2019-10-18 13:19:58 -0300

    inclusão do sistema de login e autenticação

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+// eslint-disable-next-line consistent-return
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'access denied: user unauthorized' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+    req.userId = decoded.id;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Token is invalid' });
+  }
+};
